7ea51ee893ae1c0320d1222d739a3c0ef2b3a71c
zjun <565061763@qq.com>
2018-08-09 15:38:00 +0800

    refactor

--- lib/src/main/java/com/fanwe/lib/swipemenu/adapter/SwipeMenuAdapter.java
+++ lib/src/main/java/com/fanwe/lib/swipemenu/adapter/SwipeMenuAdapter.java
@@ -7,10 +7,31 @@ import com.fanwe.lib.swipemenu.SwipeMenu;
 
 public interface SwipeMenuAdapter
 {
+    /**
+     * 创建菜单view
+     *
+     * @param position
+     * @param parent
+     * @return
+     */
     View onCreateMenuView(int position, ViewGroup parent);
 
+    /**
+     * 绑定数据
+     *
+     * @param position
+     * @param contentView
+     * @param menuView
+     * @param swipeMenu
+     */
     void onBindData(int position, View contentView, View menuView, SwipeMenu swipeMenu);
 
+    /**
+     * 返回每一项的唯一标识，建议返回每一项对应的实体对象
+     *
+     * @param position
+     * @return
+     */
     Object getTag(int position);
 
     SwipeMenuHolder getSwipeMenuHolder();
