58e0913043952ddafa89c46244be3cf40ae4f141
zjun <565061763@qq.com>
2018-12-26 15:07:22 +0800

    gesture

--- lib/src/main/java/com/sd/lib/swipemenu/gesture/FGestureManager.java
+++ lib/src/main/java/com/sd/lib/swipemenu/gesture/FGestureManager.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017 Sunday (https://github.com/zj565061763/gesture)
+ * Copyright (C) 2017 Sunday (https://github.com/zj565061763)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
