732271c1e9b2596a10b8aca43f2acc18504b2946
zjun <565061763@qq.com>
2020-09-19 11:07:37 +0800

    requestLayout() 逻辑调整

--- lib/src/main/java/com/sd/lib/swipemenu/BaseSwipeMenu.java
+++ lib/src/main/java/com/sd/lib/swipemenu/BaseSwipeMenu.java
@@ -308,8 +308,8 @@ abstract class BaseSwipeMenu extends ViewGroup implements SwipeMenu
 
         updateView(state, anim);
 
-        if (changed && state == State.Close && mScrollState == ScrollState.Idle)
-            setMenuDirection(null);
+//        if (changed && state == State.Close && mScrollState == ScrollState.Idle)
+//            setMenuDirection(null);
 
         return changed;
     }
@@ -752,6 +752,13 @@ abstract class BaseSwipeMenu extends ViewGroup implements SwipeMenu
                 } else
                 {
                     layoutInternal(state);
+                    if (state == mState)
+                    {
+                        if (mIsDebug)
+                            Log.i(SwipeMenu.class.getSimpleName(), "state == mState requestLayout()");
+
+                        requestLayout();
+                    }
                 }
             }
         }
